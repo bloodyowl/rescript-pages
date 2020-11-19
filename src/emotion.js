8dc3b28258085da16646c45e59400f0bc0be0b5d
Matthias Le Brun <mlbli@me.com>
2020-11-19 10:00:44 +0100

    Hum ok

--- src/emotion.js
+++ src/emotion.js
@@ -1,4 +1,4 @@
 let createEmotion =
   require("create-emotion").default || require("create-emotion");
 
-module.exports = createEmotion({ key: "__rpcss" });
+module.exports = createEmotion({ key: "rpcss" });
