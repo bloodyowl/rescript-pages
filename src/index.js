5b9e099c0dbe82b23d209be461e4c35743127a2e
bloodyowl <bloodyowl@icloud.com>
2021-06-05 17:35:54 +0200

    Remove log

--- src/index.js
+++ src/index.js
@@ -31,7 +31,6 @@ async function prerenderForConfig(config, mode) {
 
 function formatError(error) {
   if (Array.isArray(error)) {
-    console.log(error)
     return error.map(formatError).join("\n\n");
   }
   return (
